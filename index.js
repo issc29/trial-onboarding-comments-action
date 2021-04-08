0bb160cc269a5846e48a42e3f71f3f13508d3bdc
Isaac Cohen <issc29@gmail.com>
2021-04-08 15:10:01 -0400

    commit

--- index.js
+++ index.js
@@ -19,20 +19,41 @@ async function run() {
   const issueInfo = cardInfo.content
 
   const issueID = issueInfo.id
-  const reponame = issueInfo.repository.name
-  const orgname = issueInfo.repository.owner.login
-  const number = issueInfo.number
   const title = issueInfo.title
   const author = issueInfo.author.login
   const body = issueInfo.body
+  const labels = issueInfo.labels.nodes
   console.log(issueID)
-  console.log(reponame)
-  console.log(orgname)
-  console.log(number)
-  console.log(title)
-  console.log(author)
-  console.log(body)
-  //await functions.commentOnIssue(issueInfo.id, onboardingComment
+  
+  const isPocLinkFound = /.*(https:\/\/github\.lightning\.force\.com.*Proof_of_Concept.*view).*/.test(body)
+  const isGitHubOrgFound = /.*GitHub Organization\(s\)\*\* - `(.*)`.*/.test(body)
+
+  const regionLabels = ['region-emea', 'corporate-se-emea', 'region-apac', 'corporate-se-apac',
+    'region-east', 'region-west','region-central', 'corporate-se-amer', 'region-pubsec']
+  var type = ''
+  var region = ''
+  for(label of labels) {
+    if(label.name == 'ghec') {
+      type = "Cloud"
+    }
+    if(label.name == 'ghes') {
+      type = "Server"
+    }
+
+    if(regionLabels.includes(label.name)) {
+      region = label.name
+    }
+  }
+
+  var comment = ''
+  comment += (isPocLinkFound) ? ':white_check_mark: POC Link Found\n' : ':x: POC Not Link Found\n'
+  comment += (isGitHubOrgFound) ? ':white_check_mark: Organization Found\n' : ':x: Organization Not Found\n'
+  comment += (!type == '') ? `:white_check_mark: Type: ${type}\n` : ':x: Type Not Found\n'
+  comment += (!region == '') ? `:white_check_mark: Region Label ${region}\n` : ':x: Region Label Not Found\n'
+
+
+
+  await functions.commentOnIssue(issueInfo.id, comment)
 
 }
 
