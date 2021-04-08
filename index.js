f52c16519f80572af85ea933fc3c0f3c1c4aa7f6
Isaac Cohen <issc29@gmail.com>
2021-04-08 14:43:01 -0400

    commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const dedent = require('dedent');
 const myToken = core.getInput('github-token');
 const octokit = github.getOctokit(myToken)
 const payload = github.context.payload
-const issueID = payload.project_card.node_id
+const cardID = payload.project_card.node_id
 const onboardingComment = core.getInput('onboarding-comment');
 
 
@@ -15,9 +15,24 @@ run();
 
 async function run() {
 
-  await functions.commentOnIssue(issueID, onboardingComment)
+  const cardInfo = await functions.getIssueInfoFromProjectCard(cardID)
+  const issueInfo = cardInfo.content
 
-
+  const issueID = issueInfo.id
+  const reponame = issueInfo.repository.name
+  const orgname = issueInfo.repository.owner.login
+  const number = issueInfo.number
+  const title = issueInfo.title
+  const author = issueInfo.author.login
+  const body = issueInfo.body
+  console.log(issueID)
+  console.log(reponame)
+  console.log(orgname)
+  console.log(number)
+  console.log(title)
+  console.log(author)
+  console.log(body)
+  //await functions.commentOnIssue(issueInfo.id, onboardingComment
 
 }
 
