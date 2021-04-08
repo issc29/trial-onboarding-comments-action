8282ead0d4d465cd1aa3c979de3cfca01b7393e2
Isaac Cohen <issc29@gmail.com>
2021-04-08 15:14:46 -0400

    commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const myToken = core.getInput('github-token');
 const octokit = github.getOctokit(myToken)
 const payload = github.context.payload
 const cardID = payload.project_card.node_id
-const onboardingComment = core.getInput('onboarding-comment');
+const contactUsers = core.getInput('contact-users');
 
 
 const functionsLib = require('actions-api-functions');
@@ -25,35 +25,41 @@ async function run() {
   const labels = issueInfo.labels.nodes
   console.log(issueID)
   
-  const isPocLinkFound = /.*(https:\/\/github\.lightning\.force\.com.*Proof_of_Concept.*view).*/.test(body)
-  const isGitHubOrgFound = /.*GitHub Organization\(s\)\*\* - `(.*)`.*/.test(body)
 
-  const regionLabels = ['region-emea', 'corporate-se-emea', 'region-apac', 'corporate-se-apac',
-    'region-east', 'region-west','region-central', 'corporate-se-amer', 'region-pubsec']
-  var type = ''
-  var region = ''
-  for(label of labels) {
-    if(label.name == 'ghec') {
-      type = "Cloud"
-    }
-    if(label.name == 'ghes') {
-      type = "Server"
+  try {
+    const isPocLinkFound = /.*(https:\/\/github\.lightning\.force\.com.*Proof_of_Concept.*view).*/.test(body)
+    const isGitHubOrgFound = /.*GitHub Organization\(s\)\*\* - `(.*)`.*/.test(body)
+
+    const regionLabels = ['region-emea', 'corporate-se-emea', 'region-apac', 'corporate-se-apac',
+      'region-east', 'region-west','region-central', 'corporate-se-amer', 'region-pubsec']
+    var type = ''
+    var region = ''
+    for(label of labels) {
+      if(label.name == 'ghec') {
+        type = "Cloud"
+      }
+      if(label.name == 'ghes') {
+        type = "Server"
+      }
+
+      if(regionLabels.includes(label.name)) {
+        region = label.name
+      }
     }
 
-    if(regionLabels.includes(label.name)) {
-      region = label.name
-    }
+    var comment = ''
+    comment += `${contactUsers} :point_up: awaiting GHAS POC access to be enabled\n\n`
+    comment += (isPocLinkFound) ? ':white_check_mark: POC Link Found\n' : ':x: POC Not Link Found\n'
+    comment += (isGitHubOrgFound) ? ':white_check_mark: Organization Found\n' : ':x: Organization Not Found\n'
+    comment += (!type == '') ? `:white_check_mark: Type: ${type}\n` : ':x: Type Not Found\n'
+    comment += (!region == '') ? `:white_check_mark: Region Label ${region}\n` : ':x: Region Label Not Found\n'
+
+
+
+    await functions.commentOnIssue(issueInfo.id, comment)
+  } catch (e) {
+    console.log(e)
   }
-
-  var comment = ''
-  comment += (isPocLinkFound) ? ':white_check_mark: POC Link Found\n' : ':x: POC Not Link Found\n'
-  comment += (isGitHubOrgFound) ? ':white_check_mark: Organization Found\n' : ':x: Organization Not Found\n'
-  comment += (!type == '') ? `:white_check_mark: Type: ${type}\n` : ':x: Type Not Found\n'
-  comment += (!region == '') ? `:white_check_mark: Region Label ${region}\n` : ':x: Region Label Not Found\n'
-
-
-
-  await functions.commentOnIssue(issueInfo.id, comment)
 
 }
 
